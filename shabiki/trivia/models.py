92f5fbe329b345a7b1e12692c05fc03e50d0f782
Norman Munge <normanmunge@gmail.com>
2023-04-24 06:54:04 +0300

    Adds game and game question models

--- shabiki/trivia/models.py
+++ shabiki/trivia/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
 class Questions(models.Model):
     questionId = models.AutoField(primary_key = True)
     question = models.CharField(max_length=255)
@@ -23,3 +25,19 @@ class Answers(models.Model):
 
     def __str__(self) -> str:
         return self.answer
+    
+class Game(models.Model):
+    timer = models.BooleanField(default=False)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    level = models.IntegerField()
+
+    def __str__(self):
+        return self.level + " : " + self.question.question
+    
+class GameQuestions(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    total_marks = models.IntegerField()
+
+    def __str__(self):
+        return self.user + ' : ' + self.game.level
