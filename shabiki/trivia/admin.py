2f11d3012ca7391e02d50cd37da9e7159033638c
Norman Munge <normanmunge@gmail.com>
2023-04-24 07:01:29 +0300

    Registers all models to the admin UI

--- shabiki/trivia/admin.py
+++ shabiki/trivia/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from django.apps import apps
 
-# Register your models here.
+# trivia_models = apps.get_app_config('trivia').get_models()
+# from .models import Questions, Choices, Answers, Game, GameQuestions
+# # Register your models here.
+
+# for model in trivia_models:
+#     try:
+#         admin.site.register(model)
+#     except admin.sites.AlreadyRegistered:
+#         pass
