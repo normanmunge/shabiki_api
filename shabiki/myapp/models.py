caeb681aa9d65810fb26dab38cafee63067df99d
Norman Munge <normanmunge@gmail.com>
2023-04-24 06:52:12 +0300

    Adds game and game questions models

--- shabiki/myapp/models.py
+++ shabiki/myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
